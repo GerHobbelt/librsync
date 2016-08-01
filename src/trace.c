49cd846dd6e112d37718a0f3392906b394576978
Paul Labedan <plabedan@gmail.com>
2016-08-01 12:59:05 +0200

    Fix build with VS2015 Win64

--- src/trace.c
+++ src/trace.c
@@ -67,7 +67,7 @@ int rs_trace_level = RS_LOG_INFO;
 
 static void rs_log_va(int level, char const *fn, char const *fmt, va_list va);
 
-#if SIZEOF_SIZE_T > SIZEOF_LONG
+#if SIZEOF_SIZE_T > SIZEOF_LONG && __GNUC__
 #  warning size_t is larger than a long integer, values in trace messages may be wrong
 #endif
 
